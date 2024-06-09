17b9c0c162a999208c1499c3933094758b1412f2
Andry Marques <Andry.marquesp@gmail.com>
2024-06-09 17:07:05 -0300

    dados alterados

--- index.js
+++ index.js
@@ -13,18 +13,20 @@
 // Autor: Rommel Vieira Carneiro
 // Data: 03/10/2023
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./db/db.json')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db/db.json');
 const cors = require('cors');
 
-// Para permitir que os dados sejam alterados, altere a linha abaixo
-// colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults()
+// Permitindo que os dados sejam alterados:
+const middlewares = jsonServer.defaults({
+  readOnly: false  
+});
 
-server.use(cors())
-server.use(middlewares)
-server.use(router)
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
 server.listen(3000, () => {
-  console.log('JSON Server is running em http://localhost:3000')
-})
+  console.log('JSON Server is running em http://localhost:3000');
+});
